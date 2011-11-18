e7f225a1b042d6be4d39047a0a7a169242262df3
Esnault Frédéric <esnault.frederic@gmail.com>
2011-11-18 17:30:19 +0100

    Renamed initialBundles directory to drop-in. Reasons are : * The initial bundles loaded by the engine at startup is now the target/engine-1.0-SNAPSHOT directory, generated by maven assembly; * The drop-in directory is now watched by apache Felix FileInstall, which install&start/uninstall&stop bundles on the go, as they are placed/removed into/from the drop-in directory. (See engine/start.sh, which now gives a JVM parameter : -Dfelix.fileinstall.dir=../drop-in, which indicates to FileInstall which directory to watch)

--- engine/start.sh
+++ engine/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Dfelix.fileinstall.dir=../initialBundles -classpath target/engine-1.0-SNAPSHOT.jar:target/engine-1.0-SNAPSHOT/org.eclipse.osgi-3.6.0.v20100517.jar:target/engine-1.0-SNAPSHOT/org.eclipse.osgi.services-3.2.100.v20100503.jar org.phoenix.osgi.engine.Engine target/engine-1.0-SNAPSHOT 
+java -Dfelix.fileinstall.dir=../drop-in -classpath target/engine-1.0-SNAPSHOT.jar:target/engine-1.0-SNAPSHOT/org.eclipse.osgi-3.6.0.v20100517.jar:target/engine-1.0-SNAPSHOT/org.eclipse.osgi.services-3.2.100.v20100503.jar org.phoenix.osgi.engine.Engine target/engine-1.0-SNAPSHOT 
